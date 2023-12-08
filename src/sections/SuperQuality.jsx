58a0cdd01429f7d4a2b74b7e659cac10c5f9fb27
Abubakar kehinde <abubakarkehinde88@gmail.com>
2023-12-08 10:11:04 +0100

    upade on super-quality

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,5 +1,17 @@
 const SuperQuality = () => {
-  return <div>SuperQuality</div>;
+  return (
+    <section
+      id="about-us"
+      className="flex justify-between
+    items-center max-lg:flex-col
+    gap-10 w-full max-container"
+    >
+      <div
+        className="flex flex-1
+      flex-col"
+      ></div>
+    </section>
+  );
 };
 
 export default SuperQuality;
